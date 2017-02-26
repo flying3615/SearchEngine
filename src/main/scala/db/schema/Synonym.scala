f9998c511d1997e37f3ef25caa1e405228f90f0e
liuyufei <gabriel.liu@centling.com>
2017-02-27 12:07:48 +1300

    init H2 DB

--- src/main/scala/db/schema/Synonym.scala
+++ src/main/scala/db/schema/Synonym.scala
@@ -4,9 +4,10 @@ import slick.ast.ColumnOption.{AutoInc, PrimaryKey, Unique}
 import slick.jdbc.H2Profile.api._
 import slick.sql.SqlProfile.ColumnOption.Nullable
 
+import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
-
+import scala.concurrent.duration._
 
 /**
   * Created by liuyufei on 27/02/17.
@@ -15,6 +16,7 @@ import scala.util.{Failure, Success}
 object schema extends App {
 
   val synonyms = TableQuery[Synonym]
+
 
   // Definition of the Synonym table
   class Synonym(tag: Tag) extends Table[(Int, String, Option[Int])](tag, "Synonym") {
@@ -42,23 +44,34 @@ object schema extends App {
 
       //FK can be null?
       synonyms += (1, "hotel", Option.empty),
-      synonyms += (2, "motel", Option(1)),
-      synonyms += (3, "inn", Option(1))
+      synonyms += (2, "motel", Some(1)),
+      synonyms += (3, "inn", Some(1))
     )
 
-    db.run(setup).onComplete{
+    val setupTask = db.run(setup).andThen{
       case Success(value)=>println("init db end")
       case Failure(e) => println("init failed "+e.printStackTrace())
     }
 
-    Thread.sleep(1000)
 
-    db.run(synonyms.result).onComplete{
+
+    Await.result(setupTask,1 seconds)
+
+    //auto generate id when id=0
+    val insertTask = db.run(synonyms++=Seq((0,"test1",None),
+                                           (0,"test2",Some(1))
+                                          )
+                            )
+
+    Await.result(insertTask,1 second)
+
+    val queryTask = db.run(synonyms.result).andThen{
       case Success(value) => println(value)
       case Failure(e) => println(e.getMessage)
     }
 
-    Thread.sleep(1000)
+    Await.result(queryTask,1 seconds)
+
   } finally db.close
 
 }
