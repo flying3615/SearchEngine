e646cb7a52cf1cfe3d698bdf435f2ac88f1644ef
liuyufei <gabriel.liu@centling.com>
2017-02-27 11:54:36 +1300

    init H2 DB

--- src/main/scala/db/schema/Synonym.scala
+++ src/main/scala/db/schema/Synonym.scala
@@ -1,6 +1,8 @@
 package db
 
+import slick.ast.ColumnOption.{AutoInc, PrimaryKey, Unique}
 import slick.jdbc.H2Profile.api._
+import slick.sql.SqlProfile.ColumnOption.Nullable
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
@@ -15,13 +17,13 @@ object schema extends App {
   val synonyms = TableQuery[Synonym]
 
   // Definition of the Synonym table
-  class Synonym(tag: Tag) extends Table[(Int, String, Int)](tag, "Synonym") {
-    def id = column[Int]("SYN_ID", O.AutoInc, O.PrimaryKey)
+  class Synonym(tag: Tag) extends Table[(Int, String, Option[Int])](tag, "Synonym") {
+    def id = column[Int]("SYN_ID", AutoInc, PrimaryKey)
 
     // This is the primary key column
-    def name = column[String]("SYN_NAME")
+    def name = column[String]("SYN_NAME",Unique)
 
-    def fk_id = column[Int]("SYN_FK_ID")
+    def fk_id = column[Option[Int]]("SYN_FK_ID",Nullable)
 
     // Every table needs a * projection with the same type as the table's type parameter
     def * = (id, name, fk_id)
@@ -38,16 +40,18 @@ object schema extends App {
       // Create the tables, including primary and foreign keys
       synonyms.schema.create,
 
-      // Insert some suppliers
-      synonyms += (1, "hotel", 0),
-      synonyms += (2, "motel", 1),
-      synonyms += (3, "inn", 1)
+      //FK can be null?
+      synonyms += (1, "hotel", Option.empty),
+      synonyms += (2, "motel", Option(1)),
+      synonyms += (3, "inn", Option(1))
     )
 
     db.run(setup).onComplete{
       case Success(value)=>println("init db end")
       case Failure(e) => println("init failed "+e.printStackTrace())
     }
+
+    Thread.sleep(1000)
 
     db.run(synonyms.result).onComplete{
       case Success(value) => println(value)
