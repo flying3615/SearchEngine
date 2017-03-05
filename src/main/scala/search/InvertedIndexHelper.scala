48707b1ffdf17c353b4871c11b1e218d3450441f
liuyufei <gabriel.liu@centling.com>
2017-03-05 23:42:47 +1300

    correct!!!

--- src/main/scala/search/InvertedIndexHelper.scala
+++ src/main/scala/search/InvertedIndexHelper.scala
@@ -26,7 +26,7 @@ object InvertedIndexHelper extends App {
   }
 
 
-  def lineToTuple(line: String): Iterable[(String, Int, ArrayBuffer[Int])] = {
+  private def lineToTuple(line: String): Iterable[(String, Int, ArrayBuffer[Int])] = {
     val ss = Map.empty[String, (String, Int, ArrayBuffer[Int])]
     line.trim.split("\\W+").foreach { word =>
       ss(word) = multiIndexOf(line, word, ss.getOrElse(word, (word, 0, ArrayBuffer[Int]())))
@@ -34,7 +34,7 @@ object InvertedIndexHelper extends App {
     ss.values
   }
 
-  def multiIndexOf(line: String, word: String, acc: (String, Int, ArrayBuffer[Int])) = {
+  private def multiIndexOf(line: String, word: String, acc: (String, Int, ArrayBuffer[Int])) = {
     if (acc._1 == word) {
       val nextIndex = if (acc._3.isEmpty) line.indexOf(word) else line.indexOf(word, acc._3.last + word.size)
       val fequ = acc._2 + 1
