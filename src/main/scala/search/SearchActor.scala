82d9e47dd0c1f1d130a1db576452b018c7858168
liuyufei <gabriel.liu@centling.com>
2017-03-07 22:36:09 +1300

    add to UI action

--- src/main/scala/search/SearchActor.scala
+++ src/main/scala/search/SearchActor.scala
@@ -21,8 +21,11 @@ class SearchActor extends Actor{
 
     val invertedMap:Map[String, Map[String, (Int, ArrayBuffer[Int])]] = InvertedIndexHelper.buildupInvertedMap(paths,Stemming.doStem _)
     println(s"inverted index map = ${invertedMap.mkString("\n")}")
-    //get path List(ArrayBuffer)
-    val pathMap = words.split(",").flatMap(word=>invertedMap.get(word).map(optionMap=>optionMap.map(_._1)))
+    //get path List(ArrayBuffer) and flat it
+    val pathMap = words.split(",").flatMap{word=>
+//        Option(Map(String->(freq,indexes)))
+        invertedMap.get(word).map(optionMap=>optionMap.map(_._1))
+    }.flatten
 
     val end = System.currentTimeMillis()
     val statistic = s"search ${pathMap.size} matches took ${end-start} ms"
